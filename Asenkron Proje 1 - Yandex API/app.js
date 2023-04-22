f4c5303ea1ca308a8a6cc569c39dc2676b36ac9e
erenmulkoglu96 <riomortensen@gmail.com>
2023-04-23 00:39:20 +0300

    first commit

---
+++ Asenkron Proje 1 - Yandex API/app.js
@@ -0,0 +1,32 @@
+// Prototype, Ajax, Callback
+
+eventListeners();
+
+function eventListeners() {
+    document.getElementById("translate-form").addEventListener("submit",translateWord);
+    // Change
+    document.getElementById("language").onchange = function() {
+        // Arayüz İşlemleri
+        ui.changeUI();
+    }
+}
+
+const translate = new Translate(document.getElementById("word").value,document.getElementById("language"));
+const ui = new UI();
+
+function translateWord(e) {
+
+    translate.changeParameters(document.getElementById("word").value,document.getElementById("language").value);
+    
+    translate.translateWord(function(err,response) {
+        if(err) {
+            // Hata
+            console.log(err);
+        }
+        else {
+            ui.displayTranslate(response);
+        }
+    });
+
+    e.preventDefault();
+}
